2655ed93ad5ab9e9d41bef863af80e01f0d9ebd6
zeyneppkts <zeyneppkts28@gmail.com>
2025-12-23 15:21:46 +0300

    uçuş yönetimi classlarına attributionlar tanımlandı

--- airlineReservationAndManagementSystem/src/flightManagement/Plane.java
+++ airlineReservationAndManagementSystem/src/flightManagement/Plane.java
@@ -2,5 +2,9 @@ package flightManagement;
 
 public class Plane {
 	//planeID, planeModel, capacity (int), seatMatrix (2D array or Map structure)
+	private String planeID;
+	private String planeModel;
+	private int capacity;
+	//private HashMap<> hashmap mi kullansak daha iyi array mi?
 
 }
--- airlineReservationAndManagementSystem/src/flightManagement/Seat.java
+++ airlineReservationAndManagementSystem/src/flightManagement/Seat.java
@@ -2,5 +2,8 @@ package flightManagement;
 
 public class Seat {
 	//seatNum (String, exp. "15A"), Class (Enum: ECONOMY, BUSINESS), price, reserveStatus (boolean).
+	private String seatNum;
+	//enum için inner class mı kullanmamız gerekiyor?
+	private boolean reserveStatus;
 
 }
